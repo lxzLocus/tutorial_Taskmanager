7ec5ece138afdcca85921928f428fe72d1848da9
lxzLocus <unicorngundam03@gmail.com>
2024-04-29 11:31:10 +0900

    一応できた
    回答コードを見てこれから手直し

--- app/src/main/java/com/example/tutorial_taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_taskmanager/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.tutorial_taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tutorial_taskmanager.ui.theme.Tutorial_TaskmanagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScreenTaskCompleted(
+                        stringResource(R.string.mainText),
+                        subText = stringResource(R.string.subText)
+                    )
                 }
             }
         }
@@ -30,17 +47,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ScreenTaskCompleted(mainText: String, subText: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
+
+    ){
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+
+        )
+        Text(
+            text = mainText,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            ),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .padding(bottom = 8.dp)
+        )
+        Text(
+            text = subText,
+            fontSize = 16.sp
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    //showSystemUi = true,
+    //name = "Preview"
+    )
 @Composable
 fun GreetingPreview() {
     Tutorial_TaskmanagerTheme {
-        Greeting("Android")
+        ScreenTaskCompleted(
+            stringResource(R.string.mainText),
+            subText = stringResource(R.string.subText)
+        )
     }
 }
